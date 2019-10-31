192073647489b0349a8ac9e9bc54e6931443fd67
João Vítor Retucci <jvitor.200003@gmail.com>
2019-10-31 16:58:23 -0300

    Correções/Cards e Items adicionados

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Input from './components/input/input';
+import Cards from './components/cards/cards';
 
 function App() {
   return (
     <div className="App">
       <header>Todo app</header>
       <Input />
+      <Cards />
     </div>
   );
 }
--- src/components/cards/cards.js
+++ src/components/cards/cards.js
@@ -1,1 +1,34 @@
 import React, { Component } from 'react';
+import { FaArrowCircleRight, FaCheckCircle } from 'react-icons/fa';
+import './styles.css';
+
+export default class Cards extends Component{
+    state = {
+        todo: [],
+    }
+
+    render(){
+        return(
+            <div className="container">
+                <div className="card">
+                    <h2>To Do</h2>
+                    <ul id="todos">
+                        <li className="items">Ir na aulda de JS<button><FaArrowCircleRight className="icon" /></button></li>
+                    </ul>
+                </div>
+                <div className="card">
+                    <h2>Doing</h2>
+                    <ul id="doing">
+                        <li className="items">Revisar HTML e CSS<button><FaArrowCircleRight className="icon" /></button></li>
+                    </ul>
+                </div>
+                <div className="card">
+                    <h2>Done</h2>
+                    <ul id="done">
+                        <li className="items">Ir na reunião de projetos<button><FaCheckCircle className="icon" /></button></li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
