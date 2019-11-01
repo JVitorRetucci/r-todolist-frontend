34727bb6952eda488c1df36834de127efc710cfa
João Vítor Retucci <jvitor.200003@gmail.com>
2019-11-01 15:45:53 -0300

    Junção Cards e Input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import Input from './components/input/input';
 import Cards from './components/cards/cards';
 
 function App() {
   return (
     <div className="App">
       <header>Todo app</header>
-      <Input />
       <Cards />
     </div>
   );
--- src/components/cards/cards.js
+++ src/components/cards/cards.js
@@ -1,40 +1,64 @@
 import React, { Component } from 'react';
-import { FaArrowCircleRight, FaTimesCircle } from 'react-icons/fa';
+import { FaArrowCircleRight, FaTimesCircle, FaPlusSquare } from 'react-icons/fa';
 import './styles.css';
 
 export default class Cards extends Component{
-    state = {
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            userInput: '',
             todo: ["João", "Pedro", "Ju"],
             doing: ["001", "002"],
             done: ["teste"]
+        }
     }
         
+    addItem = (input) =>{
+        let listArray = this.state.todo;
+        
+        listArray.push(input);
+        console.log(listArray);
+        
+
+        this.setState({
+            todo: listArray,
+            userInput: ''
+        });
+    }
+
     render(){
         return(
-            <div className="container">
-                <div className="card">
-                    <h2>To Do</h2>
-                    <ul id="todos">
-                        {this.state.todo.map((item, index) => (
-                            <li key={index} className="items">{item}<button><FaArrowCircleRight className="icon" /></button></li>
-                        ))}
-                    </ul>
+            <div>
+                <div className="busca">
+                    <input type="text" className="input" placeholder="Adicione um novo to do!" />
+                    <button type="button" onClick={this.addItem}><FaPlusSquare className="icone" /></button>
                 </div>
-                <div className="card">
-                    <h2>Doing</h2>
-                    <ul id="doing">
-                        {this.state.doing.map((item, index) => (
-                            <li key={index} className="items">{item}<button><FaArrowCircleRight className="icon" /></button></li>
-                        ))}
-                    </ul>
-                </div>
-                <div className="card">
-                    <h2>Done</h2>
-                    <ul id="done">
-                        {this.state.done.map((item, index) => (
-                            <li key={index} className="items">{item}<button><FaTimesCircle className="icon" /></button></li>
-                        ))}
-                    </ul>
+                <div className="container">
+                    <div className="card">
+                        <h2>To Do</h2>
+                        <ul id="todos">
+                            {this.state.todo.map((item, index) => (
+                                <li key={index} className="items">{item}<button><FaArrowCircleRight className="icon" /></button></li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className="card">
+                        <h2>Doing</h2>
+                        <ul id="doing">
+                            {this.state.doing.map((item, index) => (
+                                <li key={index} className="items">{item}<button><FaArrowCircleRight className="icon" /></button></li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className="card">
+                        <h2>Done</h2>
+                        <ul id="done">
+                            {this.state.done.map((item, index) => (
+                                <li key={index} className="items">{item}<button><FaTimesCircle className="icon" /></button></li>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
             </div>
         )
